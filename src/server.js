d6003765d79189cefb8554456e28a0c2791e6a3b
john911120 <wtt54@naver.com>
2022-05-20 15:01:36 +0900

    git push -u origin main

--- src/server.js
+++ src/server.js
@@ -8,5 +8,8 @@ app.set("views", __dirname + "/views")
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"))
 
+//catch all url을 생성하려면 이렇게 하면 된다.
+app.get("/*", (req, res) => res.redirect("/"))
+
 const handleListen = () => console.log(`listening on http://localhost:3000`)
 app.listen(3000, handleListen)
