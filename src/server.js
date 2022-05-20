51cd8f9fbfcd6160442bcbbaa312aea661eb43dd
john911120 <wtt54@naver.com>
2022-05-20 14:12:48 +0900

    git push origin main

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 import express from 'express';
 
+// pug페이지들을 렌더링하기 위해 pug 설정한다.
 const app = express()
-console.log("hello")
-app.listen(3000)
+
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views")
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"))
+
+const handleListen = () => console.log(`listening on http://localhost:3000`)
+app.listen(3000, handleListen)
