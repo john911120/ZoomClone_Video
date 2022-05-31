3b3162cdaa555d60e6c32d444c7c109c1a0bd22f
john911120 <wtt54@naver.com>
2022-05-31 12:48:22 +0900

    git push -u origin main

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,78 +1,82 @@
  const socket = io()
+ const myFace = document.getElementById("myFace")
+ const muteBtn = document.getElementById("mute")
+ const cameraBtn = document.getElementById("camera")
+ const cameraSelect = document.getElementById("cameras")
 
- const welcome = document.getElementById("welcome")
- const room = document.getElementById("room")
- const form = welcome.querySelector("form")
+ let myStream;
+ let muted = false
+ let cameraOff = false
 
-room.hidden = true
+ async function getCameras(){
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter((device) => device.kind === "videoinput")
+        const currentCamera = myStream.getVideoTracks()[0]
+        cameras.forEach((camera) => {
+            const option = document.createElement("option")
+            option.value = camera.deviceId
+            option.innerText = camera.label
+            if(currentCamera.label == camera.label){
+                option.selected = true
+            }
+            cameraSelect.appendChild(option)
+        })
+    } catch(e){
+        console.log(e)
+    }
+ }
 
-let roomName 
+ async function getMedia(deviceId){
+    const initialConstraints = {
+        audio : true,
+        video : {facingMode : "user"}
+    }
+    const cameraConstraints = {
+        audio : true,
+        video : {deviceId: {exact: deviceId}}
+    }
+     try{
+         myStream = await navigator.mediaDevices.getUserMedia(
+         deviceId ? cameraConstraints: initialConstraints
+            )
+         myFace.srcObject = myStream
+         if(!deviceId){
+         await getCameras()
+         }
+    }catch(e){
+         console.log(e)
+     }
+ }
+getMedia()
 
-function addMessage(message) {
-    const ul = room.querySelector("ul")
-    const li = document.createElement("li")
-    li.innerText = message
-    ul.appendChild(li)
+
+function handleMuteClick() {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled))
+    if(!muted) {
+        muteBtn.innerText = "unMute"
+        muted = true
+    } else {
+        muteBtn.innerText = "Mute"
+        muted = false
+    }
 }
 
-function showRoom(){
-    welcome.hidden = true
-    room.hidden = false
-    const h3 = room.querySelector("h3")
-    h3.innerText = `Room ${roomName}`
-    const msgForm = room.querySelector("#msg")
-    const nameForm = room.querySelector("#name")
-    msgForm.addEventListener("submit", handleMessageSubmit)
-    nameForm.addEventListener("submit", handleNicknameSubmit)
+function handleCameraClick() {
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled))
+    if(!cameraOff){
+        cameraBtn.innerText = "Turn Camera On"
+        cameraOff = true
+    } else {
+        cameraBtn.innerText = "Turn Camera Off"
+        cameraOff = false
+    }
 }
 
-function handleRoomSubmit(event) {
-    event.preventDefault()
-    const input = form.querySelector("input")
-    socket.emit("enter_room", input.value, showRoom)
-    roomName = input.value
-    input.value = ""
+async function handleCameraChange(){
+    await getMedia(cameraSelect.value)
 }
 
-function handleMessageSubmit(event) {
-    event.preventDefault()
-    const input = room.querySelector("#msg input")
-    const value = input.value
-    socket.emit("new_message", input.value, roomName, () => {
-        addMessage(`you: ${value}`)
-    })
-    input.value=""
-}
-
-function handleNicknameSubmit(event){
-    event.preventDefault()
-    const input = room.querySelector("#name input")
-    socket.emit("nickname", input.value)
-}
-
-
- form.addEventListener("submit", handleRoomSubmit)
-
- socket.on("welcome", (user, newCount) => {
-    const h3 = room.querySelector("h3")
-    h3.innerText = `Room ${roomName} (${newCount})`
-    addMessage(`${user} arrived!`)
- })
-
- socket.on("bye", (left, newCount) =>{
-    const h3 = room.querySelector("h3")
-    h3.innerText = `Room ${roomName} (${newCount})`
-    addMessage(`${left} left the room`)
-})
-
-socket.on("new_message", addMessage)
-
-socket.on("room_change", (rooms) => {
-    const roomList = welcome.querySelector("ul")
-
-    rooms.forEach((room) => {
-       const li = document.createElement("li")
-       li.innerText = room
-       roomList.append(li)
-    })
-})
+muteBtn.addEventListener("click", handleMuteClick)
+cameraBtn.addEventListener("click", handleCameraClick)
+cameraSelect.addEventListener("input", handleCameraChange)
--- src/server.js
+++ src/server.js
@@ -1,10 +1,7 @@
 import express from 'express';
-import {Server} from "socket.io";
+import SocketIO from "socket.io";
 import http from 'http';
-import { count } from 'console';
-import { instrument } from '@socket.io/admin-ui';
 
-// pug페이지들을 렌더링하기 위해 pug 설정한다.
 const app = express()
 
 app.set("view engine", "pug")
@@ -15,101 +12,9 @@ app.get("/", (req, res) => res.render("home"))
 //catch all url을 생성하려면 이렇게 하면 된다.
 app.get("/*", (req, res) => res.redirect("/"))
 
-function handleConnection(socket) {
-    console.log(socket)
-}
+const httpServer = http.createServer(app)
+const wsServer = SocketIO(httpServer)
 
-const server = http.createServer(app)
-//IO서버를 생성
-const wsServer = new Server(server, {
-    cors:{
-        origin: ["https://admin.socket.io"],
-        credentials:true,
-    },
-})
-instrument(wsServer, {
-    auth:false
-})
-
-
-function publicRooms(){
-    const {
-        sockets : {
-            adapter:{sids, rooms},
-        },
-    } = wsServer
-    const publicRooms = []
-    rooms.forEach((_, key) => {
-        if(sids.get(key) === undefined){
-            publicRooms.push(key)
-        }
-    })
-    return publicRooms
-}
-
-function countRoom(roomName){
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size
-}
-
-
-wsServer.on("connection", socket => {
-    socket["nickname"] = "Anonymous"
-    socket.onAny((event) => {
-        console.log(wsServer.sockets.adapter)
-        console.log(`Socket Event : ${event}`)
-    })
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName)
-        done()
-        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName))
-        wsServer.sockets.emit("room_change", publicRooms())
-    })
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room) -1))
-        wsServer.sockets.emit("room_change", publicRooms())
-    })
-    socket.on("disconnect", () => {
-        wsServer.sockets.emit("room_change", publicRooms())
-    })
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}:${msg}`)
-        done()
-    })
-    socket.on("nickname", (nickname) => (socket["nickname"] = nickname))
-})
-
-
-/* 웹 소켓을 사용한 코드
-// import WebSocket from 'ws';
-
-//const wss = new WebSocket.Server({server})
-
-const sockets = []
-
-
-wss.on("connection", (socket) => {
-    sockets.push(socket)
-    socket["nickname"] = "Anonymous"
-    console.log("Connected to Browser ✅")
-    socket.on("close", () => {
-        console.log("Disconnected from the Client ❌")
-    })
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg)
-       // console.log(message.type, message.payload)
-       // sockets.forEach(aSocket => aSocket.send(`${message}`))
-
-       switch(message.type){
-           case "new_message":
-               sockets.forEach(aSocket => aSocket.send(`${socket.nickname}:${message.payload}`))
-               break
-           case "nickname":
-               socket["nickname"] = message.payload
-       }
-      //  socket.send(message)
-    })
-})
-*/
 
 const handleListen = () => console.log(`listening on http://localhost:3001`)
-server.listen(3001, handleListen)
+httpServer.listen(3001, handleListen)
